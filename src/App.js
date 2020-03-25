2206f5605717a0772c0668489e4b740efa268209
goldapple <buc02266@naver.com>
2020-03-25 21:47:50 +0900

    add express
    express 라이브러리를 통한 웹서버 동시 진행

--- src/App.js
+++ src/App.js
@@ -10,9 +10,19 @@ import {
 import { Route } from "react-router-dom";
 
 class App extends Component {
+  state = {
+    username: null
+  };
+  componentDidMount() {
+    fetch("http://localhost:3001/api")
+      .then(res => res.json())
+      .then(data => this.setState({ username: data.username }));
+  }
   render() {
+    const username = this.state.username;
     return (
       <div className="App">
+        {username ? `Hello ${username}` : "Hello World"}
         <Route exact path="/" component={MainPage} />
         <Route path="/speakerPage" component={SpeakerPage} />
         <Route path="/scriptSave" component={ScriptSave} />
