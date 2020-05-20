dd88e11321fee7836ca28c81348a879505355377
goldapple <buc02266@naver.com>
2020-05-20 20:28:46 +0900

    cors error fix

--- servers/clientServer.js
+++ servers/clientServer.js
@@ -21,7 +21,7 @@ var nowPlayType = null;
 var rollXml = null;
 var playerListXml = null;
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://127.0.0.1:3000", credentials: true }));
 fs.readFile(__dirname + "/xml/ip.xml", "utf8", function (err, data) {
   //서버 개설 시 파일 읽기
 
